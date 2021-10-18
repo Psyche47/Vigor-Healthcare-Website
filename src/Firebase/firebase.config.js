c52279818c41b3551ab85a197d8eb8f5f0ead22d
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-18 22:50:25 +0600

    Added firebase config

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWObJMkkui1i1-HTX0OBtnvV15aU9T1c8",
+  authDomain: "vigor-general-hospital.firebaseapp.com",
+  projectId: "vigor-general-hospital",
+  storageBucket: "vigor-general-hospital.appspot.com",
+  messagingSenderId: "1091726167032",
+  appId: "1:1091726167032:web:96ad998bbcbbee7e7b240d",
+};
+
+export default firebaseConfig;
