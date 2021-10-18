bfb9b967ad10038bf1f950a55114d13d3d9fb49b
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-19 02:24:45 +0600

    Components added, favicon updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home";
+import Services from "./Pages/Services/Services";
+import Header from "./Shared/Header";
+import Footer from "./Shared/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const serviceDetails = () => {
+  return (
+    <div>
+      <h2>Service Details.</h2>
+    </div>
+  );
+};
+
+export default serviceDetails;
