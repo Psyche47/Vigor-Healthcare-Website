e66829de5cdcb693344c0b4b7090410f54174dc9
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-18 23:02:50 +0600

    Implemented Firebase Authentication

---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,46 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import initializeAuthentication from "../Firebase/firebase.init";
+
+initializeAuthentication();
+
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
+  const googleProvider = GoogleAuthProvider();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider);
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+  }, []);
+
+  const logOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    });
+  };
+
+  return {
+    user,
+    signInWithGoogle,
+    logOut,
+  };
+};
+
+export default useFirebase;
